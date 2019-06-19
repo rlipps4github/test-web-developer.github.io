aafc9af435d7952c1d5e87d94aaad36989534e79
Ron Lipps <ronlipps@rons-imac27.attlocal.net>
2019-06-19 13:10:17 -0700

    finalize aside and section - fix any bugs

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -9,25 +9,43 @@ const theGallery = document.querySelector('#gallery');
 
 // MOBILE NAV BUTTON
 
+theNavBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    theNav.classList.toggle('expand');
+    theNavBtn.classList.toggle('expand');
+});
 
+// FAKE AN ACTIVE NAV
 
-// MAIN NAV HOVER
+theNav.addEventListener('click', (e) => {
+    var elems = document.querySelector('.active');
+    if(elems !==null) elems.classList.remove('active');
+    if (e.target.tagName === 'A') e.target.parentNode.classList.add('active');
+});
 
+// RESIZE RESET
 
+window.addEventListener('resize', () => {
+    theNav.classList.remove('expand');
+    theNavBtn.classList.remove('expand'); 
+    theGallery.classList.remove('mobile');  
+    if (window.innerWidth < 600) theGallery.classList.add('mobile'); 
+});
 
 // BUILD THE GALLERY IMAGES
 
 let imgHTML = '';
 for (var img in images) {
     imgHTML += `
-    <div id="${img}" data-head="${images[img].heading}" data-desc="${images[img].description}">
-        <img src="${images[img].path}" />
-    </div>
+    <figure id="${img}" data-head="${images[img].heading}" data-desc="${images[img].description}">
+        <div class="imageWrap"><img src="public/${images[img].path}" /></div>
+        <figcaption>${images[img].heading} - ${images[img].description}</figcaption>
+    </figure>
     `;
 }
 theGallery.innerHTML = imgHTML;
 
-// GALLERY HOVER
+
 
 
 
